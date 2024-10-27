412c8929e830e43f16cd352e4d8934ba7ea41b6f
Arseniy Semenov <_senyaa@tutanota.com>
2024-10-28 01:11:49 +0300

    refactoring

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -1,2 +1,21 @@
 #include <stdio.h>
+#include "arch.h"
 
+/*
+	read binary
+	initialize registers
+	initialize stack and call stack
+	(maybe start from main label)
+	implement all instructions
+	draw instruction draws on framebuffer
+	
+
+
+	*/
+
+int main(int argc, char** argv)
+{
+	
+
+	return 0;
+}
